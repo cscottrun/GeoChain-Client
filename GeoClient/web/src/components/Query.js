c25f7f846f0d7500daddd79bd0b703fa3e1fb7d2
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2019-02-01 12:02:02 -0600

    can sort by all 4 properties

--- GeoClient/web/src/components/Query.js
+++ GeoClient/web/src/components/Query.js
@@ -9,9 +9,13 @@ export default class Query extends Component {
     this.state={
       data: [],
     }
+    this.sortByCode = this.sortByCode.bind(this)
+    this.sortByTime = this.sortByTime.bind(this)
+    this.sortByLat = this.sortByLat.bind(this)
+    this.sortByLong = this.sortByLong.bind(this)
+
   }
 
-  
   componentWillMount(){
     return fetch('https://geochain-server.herokuapp.com/entries')
       .then((res) => res.json())
@@ -26,7 +30,49 @@ export default class Query extends Component {
       });
   }
 
+  sortByCode () {
+    function sortCallback (a,b) {
+      if (a.barcode_data < b.barcode_data) return -1;
+      if (a.barcode_data > b.barcode_data) return 1;
+      return 0;
+    }
+    let sorted = Object.values(this.state.data).sort(sortCallback);
+    this.setState({data: sorted})
+  }
+
+  sortByTime () {
+    function sortCallback (a,b) {
+      if (a.time_scanned_unix < b.time_scanned_unix) return -1;
+      if (a.time_scanned_unix > b.time_scanned_unix) return 1;
+      return 0;
+    }
+    let sorted = Object.values(this.state.data).sort(sortCallback);
+    this.setState({data: sorted})
+  }
+
+  sortByLat () {
+    function sortCallback (a,b) {
+      if (a.latitude < b.latitude) return -1;
+      if (a.latitude > b.latitude) return 1;
+      return 0;
+    }
+    let sorted = Object.values(this.state.data).sort(sortCallback);
+    this.setState({data: sorted})
+  }
+
+  sortByLong () {
+    function sortCallback (a,b) {
+      if (a.longitude < b.longitude) return -1;
+      if (a.longitudea > b.longitude) return 1;
+      return 0;
+    }
+    let sorted = Object.values(this.state.data).sort(sortCallback);
+    this.setState({data: sorted})
+  }
+
   render() {
+
+    
    
     let entries = this.state.data.map( entry => {
       return < Entry entry={entry}  key={entry.id} />
@@ -38,10 +84,10 @@ export default class Query extends Component {
       <table className="u-full-width">
         <thead>
           <tr>
-            <th>Barcode</th>
-            <th>Time Scanned</th>
-            <th>Latitude</th>
-            <th>Longitude</th>
+            <th onClick={this.sortByCode}>Barcode</th>
+            <th onClick={this.sortByTime}>Time Scanned</th>
+            <th onClick={this.sortByLat}>Latitude</th>
+            <th onClick={this.sortByLong}>Longitude</th>
             <th>Factom Verify</th>
           </tr>
         </thead>
