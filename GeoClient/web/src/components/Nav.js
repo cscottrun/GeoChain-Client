bb62189fdc2a9bf4ddfd85bb8f0219f769283d6d
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2019-02-04 13:02:45 -0600

    remove yarn.lock

--- GeoClient/web/src/components/Nav.js
+++ GeoClient/web/src/components/Nav.js
@@ -7,7 +7,7 @@ export default class Nav extends Component {
   render() {
     return (
       <div className='navBar'>
-        <img src={require('../logo.png')} className='logo'/>
+        <img src={require('../logo.png')} className='logo' alt='logo'/>
         <ul className='navList'>
           <li className='navOpt' onClick={this.props.navigate.bind(this, 'NewChain')}>New Chain</li>
           <li className='navOpt' onClick={this.props.navigate.bind(this, 'Query')}>Explore Entries</li>
